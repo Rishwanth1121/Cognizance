7e3edc225eed723aa6fdc0659c123df30c6f0757
Rishwanth1121 <99713609+Rishwanth1121@users.noreply.github.com>
2022-02-23 22:23:46 +0530

    Delete Task 4 directory

--- Task 4/Question1.py
+++
@@ -1,4 +0,0 @@
-#Rishwanth M
-number1=int(input("Enter The First Number: "))
-number2=int(input("Enter The Second Number: "))
-print("Sum= ",(number1+number2))
--- Task 4/Question2.py
+++
@@ -1,5 +0,0 @@
-#Rishwanth M
-string=input("Enter any String:")
-l=len(string)
-for i in range (0,l,2):
-    print(string[i],end="")
